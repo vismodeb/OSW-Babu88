d4c83470931b6f20ed5f6260fb1bde4fbed47a4b
vismodev <vismodeb2000@gmail.com>
2024-01-14 13:11:34 +0600

    Banner slider area add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
+// SWIPER JS FULL image banner
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  effect: "fade",
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
 
+// language click flag image change
 document.addEventListener('DOMContentLoaded', function() {
   var imageSelector = document.getElementById('imageSelector');
   var imageContainer = document.getElementById('imageContainer');
@@ -20,3 +49,4 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   });
 });
+
